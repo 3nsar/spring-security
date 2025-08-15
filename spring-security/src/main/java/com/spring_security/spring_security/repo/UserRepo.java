d56c22ed9833327d411ba14786a4b826d997d5be
Ensar <ensar_mehmedovic@hotmail.com>
2025-08-15 18:23:47 +0200

    commit

--- spring-security/src/main/java/com/spring_security/spring_security/repo/UserRepo.java
+++ spring-security/src/main/java/com/spring_security/spring_security/repo/UserRepo.java
@@ -1,6 +1,8 @@
 package com.spring_security.spring_security.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository {
 }
--- spring-security/src/main/java/com/spring_security/spring_security/service/MyUserDetailsService.java
+++ spring-security/src/main/java/com/spring_security/spring_security/service/MyUserDetailsService.java
@@ -1,10 +1,18 @@
 package com.spring_security.spring_security.service;
 
+import com.spring_security.spring_security.repo.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MyUserDetailsService implements UserDetailsService {
+
+    private UserRepo userRepo;
+
+    public MyUserDetailsService(UserRepo userRepo){
+        this.userRepo = userRepo;
+    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
