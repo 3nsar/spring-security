77c88aac8630fe9c86ab0465a9ec411356916d07
Ensar <ensar_mehmedovic@hotmail.com>
2025-08-18 09:20:28 +0200

    commit

--- spring-security/src/main/java/com/spring_security/spring_security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/spring_security/spring_security/config/SecurityConfig.java
@@ -5,9 +5,11 @@ import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +26,6 @@ public class SecurityConfig {
     private UserDetailsService userDetailsService;
 
     @Bean
-
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
@@ -48,4 +49,13 @@ public class SecurityConfig {
 
         return provider;
     }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+
 }
